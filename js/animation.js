f1158149eae0b90d3665085e4f244239e9bce47c
SuperLucky <mail.superlucky@gmail.com>
2021-02-21 07:50:47 +0800

    add Edge

--- js/animation.js
+++ js/animation.js
@@ -12,12 +12,14 @@ var IE = eval('"v"=="\v"');
 function rotate1()
 {
     let choise = Math.floor(Math.random()*8+1);
+    let loop = Math.floor(Math.random()*3+3);
     
     jQuery('#Rotate').rotate({
         'angle' : 0,
-        'animateTo' : 382.5 + 3600 - 45*choise,
-        'duration': 5000
+        'animateTo' : 360*loop - 45*choise + 45,
+        'duration': 1200*(loop+1),
+        'callback': function(){alert("抽到奖品 "+ choise.toString());}
     });
 
-    alert("抽到奖品 "+ choise.toString());
+    
 }
